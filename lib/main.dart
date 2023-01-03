d80b3e10a7d5f8dd4f72bedf5cfd18d8012a096f
mytrofanov <mytrofanovmaxym@gmail.com>
2023-01-03 11:27:04 +0200

    feat: routes

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const FormWidget(),
         '/result': (context) => const ResultWidget(),
       },
     );
